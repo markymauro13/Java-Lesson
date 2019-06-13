416c642951565bc1832a669359d2619c85c25b1b
Mark Mauro <markymauro@gmail.com>
2019-06-12 21:28:06 -0400

    Update CLICK_HERE.java

--- CLICK_HERE.java
+++ CLICK_HERE.java
@@ -1,1 +1,19 @@
+import javax.swing.*;
 
+public class Window {
+	
+	private static JButton button;
+	private static JTextField textField;
+  
+  // methods .setBounds(x, y, width ,height) and .add(Compenent c) must be used to set and add the Object to the JFrame
+  // ex: button.setBounds(100,100,300,300)...
+  
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("This is a window");	// creates the JFrame and titles it
+		
+		frame.setSize(400,400);	// sets the size of the frame
+    frame.setLayout(null);	// sets layout   
+		frame.setVisible(true);	// sets JFrame to be visible
+	
+	}
+}
